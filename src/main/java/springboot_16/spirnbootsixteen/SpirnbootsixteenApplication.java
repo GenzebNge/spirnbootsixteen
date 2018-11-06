346cea9914d884e9ca524adadf88a0681b5abc04
GBTC440004UR <genzebnge@gmail.com>
2018-11-06 16:34:45 -0500

    Initial commit

---
+++ src/main/java/springboot_16/spirnbootsixteen/SpirnbootsixteenApplication.java
@@ -0,0 +1,12 @@
+package springboot_16.spirnbootsixteen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirnbootsixteenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpirnbootsixteenApplication.class, args);
+	}
+}
